3e200a4afeb10e71f4689923c89c1b44ca7ed29d
Alex Tomjack <amt1309@gmail.com>
2024-02-07 02:09:43 -0600

    Added sound cues and error reading

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,9 @@ import * as vscode from "vscode";
 import { window } from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
+  let prevLineNumber = vscode.window.activeTextEditor?.selection.active.line || -1;
+
+  //Read line number upon focusing on document
   context.subscriptions.push(
     window.onDidChangeActiveTextEditor((editor) => {
       if (editor) {
@@ -17,6 +20,7 @@ export function activate(context: vscode.ExtensionContext) {
     })
   );
 
+  //Play sound with varying pitch depending on indentation
   context.subscriptions.push(
     window.onDidChangeTextEditorSelection((event) => {
       const editor = event.textEditor;
@@ -24,25 +28,53 @@ export function activate(context: vscode.ExtensionContext) {
       const line = editor.document.lineAt(editor.selection.active.line);
       const indentChar = line.text.charAt(0);
       const leadingSpace = line.text.length - line.text.trimStart().length;
-      console.log(line.text);
-      let indent;
+      let indent = 0;
       if (indentChar === "\t") {
         // used tabs
-        console.log(`leading: ${leadingSpace}`);
         indent = leadingSpace;
       } else if (indentChar === " ") {
         // used spaces
-        console.log(`leading: ${Math.ceil(leadingSpace / (editor.options.tabSize! as number))}`);
         indent = Math.ceil(leadingSpace / (editor.options.tabSize! as number));
-      } else {
-        indent = 0;
       }
-      console.log(`indent: ${indent}`);
+      if (indent === 0) return;
       const outputChannel = jzz().openMidiOut().or("error");
       outputChannel.note(0, 10 + 10 * indent, 127, 100);
     })
   );
 
+  //Play sound when user goes on same line as an error.
+  context.subscriptions.push(
+    window.onDidChangeTextEditorSelection((event) => {
+      const editor = event.textEditor;
+      if (editor !== window.activeTextEditor) return;
+      const currLine = editor.selection.active.line;
+      let diagnostics = vscode.languages.getDiagnostics(editor.document.uri).filter((e) => {
+        return e.range.start.line === currLine;
+      });
+      console.log(`diagnostics: ${diagnostics.length} prevLine: ${prevLineNumber} curr: ${currLine}`);
+      if (diagnostics.length > 0 && currLine !== prevLineNumber) {
+        const outputChannel = jzz().openMidiOut().or("error");
+        outputChannel.program(1, 81);
+        outputChannel.note(1, 50, 127, 100);
+        const lineDisposable = window.onDidChangeTextEditorSelection((event) => {
+          const editor = event.textEditor;
+          if (editor !== window.activeTextEditor) return;
+          if (editor.selection.active.line !== currLine) {
+            lineDisposable.dispose();
+            commandDisposable.dispose();
+          }
+        });
+        const commandDisposable = vscode.commands.registerCommand("audible-code.ReadErrors", async () => {
+          for (const diagnostic of diagnostics) {
+            await OutputTTS(diagnostic.message);
+          }
+        });
+      }
+      prevLineNumber = currLine;
+    })
+  );
+
+  //Register command to read current line
   let disposable = vscode.commands.registerCommand("audible-code.ReadLine", () => {
     const editor = window.activeTextEditor;
     if (editor) {
@@ -57,5 +89,7 @@ export function activate(context: vscode.ExtensionContext) {
 export function deactivate() {}
 
 function OutputTTS(message: string) {
-  say.speak(message);
+  return new Promise((res, rej) => {
+    say.speak(message, undefined, undefined, res);
+  });
 }
